fd5ee6d6cd2fc80730452eb6ace3983a46269c58
alrdebugne <alrdebugne@gmail.com>
2021-05-19 23:23:50 +0200

    Adds socket.io to listen to events

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
+var { Server } = require("socket.io");
+var io = new Server(server);
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+    console.log('A user connected');
+    socket.on('disconnect', function() {
+        console.log('User disconnected');
+    });
 });
 
 server.listen(8081, function() {
